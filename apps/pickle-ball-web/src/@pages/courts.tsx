4f8890fbb1c6b52555085f263c973fe872eb64dd
sathishd_recode <sathishkumar.durairaj@kamerai.ai>
2024-09-08 20:08:25 +0530

    e

--- apps/pickle-ball-web/src/@pages/courts.tsx
+++ apps/pickle-ball-web/src/@pages/courts.tsx
@@ -8,64 +8,11 @@ export default function AceCourts() {
     const stepperRef = useRef(null);
 
     return (
-    <div className="flex justify-content-center">
-        <Stepper ref={stepperRef}>
-            <StepperPanel header="Header I">
-                <div className="d-flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                        <img src="./Court_01.png" alt="Court 1" style={{width:"60%"}}/>
-                    </div>
-                </div>
-                <div className="d-flex pt-4 justify-content-end">
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel header="Header II">
-                <div className="d-flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <img src="./Court_02.png" alt="Court 1" style={{width:"60%"}}/>
-                    </div>
-                </div>
-                <div className="d-flex pt-4 justify-content-between">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel header="Header III">
-                <div className="d-flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <img src="./Court_03.png" alt="Court 1" style={{width:"60%"}}/>
-                    </div>
-                </div>
-                <div className="d-flex pt-4 justify-content-between">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel header="Header IV">
-                <div className="d-flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <img src="./Court_04.png" alt="Court 1" style={{width:"60%"}}/>
-                    </div>
-                </div>
-                <div className="d-flex pt-4 justify-content-between">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={() => stepperRef.current.nextCallback()} />
-                </div>
-            </StepperPanel>
-            <StepperPanel header="Header V">
-                <div className="d-flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
-                    <img src="./Court_05.png" alt="Court 1" style={{width:"60%"}}/>
-                    </div>
-                </div>
-                <div className="d-flex pt-4 justify-content-between">
-                    <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <button className="btn btn-primary" type="submit" data-bs-dismiss="modal">Save Court</button>
-                </div>
-            </StepperPanel>
-        </Stepper>
-    </div>
+        <div className="p-fluid">
+            <div className="card">
+              
+            </div>
+        </div>
     )
 }
         
